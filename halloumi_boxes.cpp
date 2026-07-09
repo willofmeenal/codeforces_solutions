b4c07ae7ae71f3afb0190956ab06ab38eb63cb1d
M MEENAL RAJ <mmeenalraj2@gmail.com>
2026-07-09 20:46:35 +0530

    Add files via upload

---
+++ halloumi_boxes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // Check if the array is already sorted
+    if (is_sorted(a.begin(), a.end()) || k >= 2) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
